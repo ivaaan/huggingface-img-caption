b946db87fc159d7831e33187a159afbfc87727b0
Ivan Afanasev <diykarelia@gmail.com>
2023-08-17 11:29:35 -0700

    readme, cleanup

---
+++ README.md
@@ -0,0 +1,9 @@
+A server-side Edge Function for Supabase that uses Hugging Face Inference API to auto-generate image captions (alt text) using open AI/ML model `vit-gpt2-image-captioning`.
+
+It runs every time there's a new image in the storage bucket.
+
+Docs:
+
+https://huggingface.co/tasks/image-to-text
+
+https://supabase.com/docs/guides/ai/hugging-face
--- supabase/functions/huggingface-img-caption/index.ts
+++ supabase/functions/huggingface-img-caption/index.ts
@@ -1,13 +1,7 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { HfInference } from "https://esm.sh/@huggingface/inference@2.2.0"
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.7.1";
 import { Database } from "./types.ts"
-
-console.log("Hello from `huggingface-img-caption` function!")
 
 const hf = new HfInference();
 
@@ -34,24 +28,14 @@ serve(async (req) => {
 
   const { signedUrl } = data;
 
-  console.log('signedUrl=', signedUrl)
-
   // run image captioning with HuggingFace
   const imgDesc: { generated_text: string} = await hf.imageToText({
     data: await(await fetch(signedUrl)).blob(),
     model: 'nlpconnect/vit-gpt2-image-captioning',
   })
 
-  console.log('imgDesc=', imgDesc)
-
   // store img caption in db table
   await supabaseAdminClient.from('image-caption').insert({id: soRecord.id!, caption: imgDesc.generated_text}).throwOnError();
 
   return new Response('ok')
 })
-
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
